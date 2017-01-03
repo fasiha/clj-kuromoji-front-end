82981cee9a64af366001dd31aa65f107dbdd2f1a
Ahmed Fasih <wuzzyview@gmail.com>
2017-01-03 00:54:37 -0500

    No tests

--- test/clojure_kuromoji/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-kuromoji.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-kuromoji.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
